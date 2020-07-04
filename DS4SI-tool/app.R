5223c6ea97c74f591fdadad2d13edae55628f594
joemarlo <joe.marlo.09@gmail.com>
2020-07-04 16:51:20 -0400

    added more plots, more filtering functionality, fixed filtering issue with table

--- DS4SI-tool/app.R
+++ DS4SI-tool/app.R
@@ -2,7 +2,8 @@ library(tidyverse)
 library(shiny)
 library(shinyWidgets)
 library(DT)
-library(plotly)
+library(gridExtra)
+options(scipen = 999)
 
 # todo --------------------------------------------------------------------
 
@@ -19,100 +20,67 @@ library(plotly)
     # although should just have weights
 # this dashboard could be useful
     # https://rstudio.github.io/shinydashboard/
+# table should only be filtered once and then used for everything else
+# should there be an advanced plotting tab? 
 
 # load data ---------------------------------------------------------------
 
-sites.DF <- read_csv("jpta_cleaned.csv")
-
+original.DF <- read_csv("jpta_cleaned.csv")
+sites.DF <- original.DF
 
 # joe's assignment code ---------------------------------------------------
+# 
+# #function "scales" the data between 0:1
+# calc.proportion <- function(distribution){
+#     (distribution - min(distribution)) / (max(distribution) - min(distribution))
+# }
+# 
+# C.sites.DF <- sites.DF %>% select(site_id, cost, unemp, pct_hs, income, comfort)
+# proportions <- apply(X = C.sites.DF, MARGIN = 2, FUN = calc.proportion)
+# proportions <- proportions[, 2:ncol(proportions)]
+# colnames(proportions) <- c("cost.P", "unemp.P", "pct_hs.P", "income.P", "comfort.P")
+# proportions <- as.data.frame(proportions)
+# 
+# #need to make sure percentiles are ordered correctly; high income proportion may mean more income
+# # cost: down
+# # unemp: up
+# # pct_hs: down
+# # income: down
+# # comfort: up
+# 
+# #categories to apply the weight against is determined by position in DF
+# weights <- c(1.25, 1.5, 1, 1.5, 1)
+# 
+# #reverse percentiles for relevant categories; 
+# #    sum P to generate a single score based on weights
+# #    bind back to original DF
+# sites.ranked.DF <- proportions %>%
+#     mutate(cost.P = 1 - cost.P,
+#            pct_hs.P = 1 - pct_hs.P,
+#            income.P = 1 - income.P) %>%
+#     rowwise() %>%
+#     # mutate(site_score = sum(c(cost.P, unemp.P, pct_hs.P, income.P, comfort.P))) %>%
+#     mutate(site_score = sum(c(cost.P, unemp.P, pct_hs.P, income.P, comfort.P) * weights)) %>%
+#     cbind(sites.DF, .) %>%
+#     ungroup()
+# 
+# #top sites
+# top.sites.DF <- sites.ranked.DF %>%
+#     filter(other_prog == F) %>% #remove sites that have another program
+#     arrange(desc(site_score)) %>%
+#     mutate(Exp_site_ct = cummean(comfort)*row_number()) %>%
+#     filter(Exp_site_ct <= 35) #filters the DF so we expect that we'll have 35 sites after accounting for comfort
+# 
+# #total expected sites
+# mean(top.sites.DF$comfort) * nrow(top.sites.DF)
+# 
+# #add identifier for top site to the main DF
+# sites.ranked.DF$top_site <- sites.ranked.DF$site_id %in% top.sites.DF$site_id
+# rm(C.sites.DF, proportions, top.sites.DF, weights)
 
-#remove outliers
-###need to throw out site 188###
-sites.DF[which.min(sites.DF$distance),]
-sites.DF <- sites.DF[sites.DF$site_id != 6573,]
-
-#function "scales" the data between 0:1
-calc.proportion <- function(distribution){
-    (distribution - min(distribution)) / (max(distribution) - min(distribution))
-}
-
-C.sites.DF <- sites.DF %>% select(site_id, cost, unemp, pct_hs, income, comfort)
-proportions <- apply(X = C.sites.DF, MARGIN = 2, FUN = calc.proportion)
-proportions <- proportions[, 2:ncol(proportions)]
-colnames(proportions) <- c("cost.P", "unemp.P", "pct_hs.P", "income.P", "comfort.P")
-proportions <- as.data.frame(proportions)
-
-#need to make sure percentiles are ordered correctly; high income proportion may mean more income
-# cost: down
-# unemp: up
-# pct_hs: down
-# income: down
-# comfort: up
-
-#categories to apply the weight against is determined by position in DF
-weights <- c(1.25, 1.5, 1, 1.5, 1)
-
-#reverse percentiles for relevant categories; 
-#    sum P to generate a single score based on weights
-#    bind back to original DF
-sites.ranked.DF <- proportions %>%
-    mutate(cost.P = 1 - cost.P,
-           pct_hs.P = 1 - pct_hs.P,
-           income.P = 1 - income.P) %>%
-    rowwise() %>%
-    # mutate(site_score = sum(c(cost.P, unemp.P, pct_hs.P, income.P, comfort.P))) %>%
-    mutate(site_score = sum(c(cost.P, unemp.P, pct_hs.P, income.P, comfort.P) * weights)) %>%
-    cbind(sites.DF, .) %>%
-    ungroup()
-
-#top sites
-top.sites.DF <- sites.ranked.DF %>%
-    filter(other_prog == F) %>% #remove sites that have another program
-    arrange(desc(site_score)) %>%
-    mutate(Exp_site_ct = cummean(comfort)*row_number()) %>%
-    filter(Exp_site_ct <= 35) #filters the DF so we expect that we'll have 35 sites after accounting for comfort
-
-#total expected sites
-mean(top.sites.DF$comfort) * nrow(top.sites.DF)
-
-#add identifier for top site to the main DF
-sites.ranked.DF$top_site <- sites.ranked.DF$site_id %in% top.sites.DF$site_id
-rm(C.sites.DF, proportions, top.sites.DF, weights)
-
-final_table <- sites.DF
+final_table <- original.DF
 
 # app ----------------------------------------------------------------------
-
-
-# shinyApp(
-#     ui = fluidPage(
-#         tabsetPanel(
-#             tabPanel("Filters", fluid = TRUE,
-#                      sidebarLayout(
-#                          sidebarPanel(selectInput("Country", "Select Country", choices = "", selected = "")),
-#                          mainPanel(
-#                              htmlOutput("Attacks")
-#                          )
-#                      )
-#             ),
-#             tabPanel("Weights", fluid = TRUE,
-#                      sidebarLayout(
-#                          sidebarPanel(sliderInput("year", "Year:", min = 1968, max = 2009, value = 2009, sep='')),
-#                          mainPanel(fluidRow(
-#                              column(7,  plotlyOutput("")),
-#                              column(5, plotlyOutput(""))   
-#                          )
-#                          )
-#                      )
-#             )
-#         )
-#     ), 
-#     server = function(input, output) {
-#         
-#     }
-# )
-
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -146,48 +114,52 @@ ui <- fluidPage(
     chooseSliderSkin(skin = "Flat",
                      color = "#5c5980"),
 
-    # text output of title with probability results
-    # htmlOutput("probability_results"),
-    # htmlOutput("probability_results_scroll"),
-
+    # <br> for spacing
     br(),
 
     sidebarLayout(
         sidebarPanel(
             
+            # text output of title with number of sites selected
+            htmlOutput("n_sites_selected"),
+            
             tabsetPanel(type = 'tabs',
-            tabPanel("Filters",
-                     br(),
-            # these should be reactive -> move to server side?
-            selectInput("region_slider", "Region:", multiple = TRUE,
-                        choices = unique(as.character(final_table$region)),
-                        selected = unique(as.character(final_table$region))),
-            selectInput("urban_slider", "Urban:", multiple = TRUE,
-                        choices = rev(unique(as.character(final_table$urban))),
-                        selected = rev(unique(as.character(final_table$urban)))),
-            selectInput("other_program_slider", "Other program available:", multiple = TRUE,
-                        choices = rev(unique(as.character(final_table$other_prog))),
-                        selected = rev(unique(as.character(final_table$other_prog)))),
-            sliderInput("unemp_slider", "Unemployment: ", min = 0.05, max = 0.15,
-                        value = c(0.05, 0.15), step = 0.01),
-            sliderInput("hs_slider", "High school graduation rate: ", min = 0.5, max = 0.9,
-                        value = c(0.5, 0.9), step = 0.01),
-            sliderInput("income_slider", "Income: ", min = 40, max = 75,
-                        value = c(40, 75), step = 1),
-            sliderInput("comfort_slider", "Comfort: ", min = 0.08, max = 0.95,
-                        value = c(0.08, 0.95), step = 0.01),
-            sliderInput("cost_slider", "Cost: ", min = 770, max = 3100,
-                        value = c(770, 3100), step = 10)
-            ),
             
-            
-            tabPanel("Weights",
+                # tab one containing the filters
+                tabPanel("Filters",
                      br(),
-            sliderInput("slider1", "Unemployment: ", min = 0, max = 100, value = 0, step = 1),
-            sliderInput("slider2", "High school graduation rate: ", min = 0, max = 100, value = 0, step = 1),
-            sliderInput("slider3", "Income: ", min = 0, max = 100, value = 0, step = 1),
-            sliderInput("slider4", "Comfort: ", min = 0, max = 100, value = 0, step = 1),
-            sliderInput("slider4", "Site cost: ", min = 0, max = 100, value = 0, step = 1)
+                    # these should be reactive -> move to server side?
+                    selectInput("region_slider", "Region:", multiple = TRUE,
+                                choices = unique(as.character(final_table$region)),
+                                selected = unique(as.character(final_table$region))),
+                    selectInput("urban_slider", "Urban:", multiple = TRUE,
+                                choices = rev(unique(as.character(final_table$urban))),
+                                selected = rev(unique(as.character(final_table$urban)))),
+                    selectInput("other_program_slider", "Other program available:", multiple = TRUE,
+                                choices = rev(unique(as.character(final_table$other_prog))),
+                                selected = rev(unique(as.character(final_table$other_prog)))),
+                    sliderInput("unemp_slider", "Unemployment: ", min = 0.05, max = 0.15,
+                                value = c(0.05, 0.15), step = 0.01),
+                    sliderInput("hs_slider", "High school graduation rate: ", min = 0.5, max = 0.9,
+                                value = c(0.5, 0.9), step = 0.01),
+                    sliderInput("income_slider", "Income: ", min = 40, max = 75,
+                                value = c(40, 75), step = 1),
+                    sliderInput("comfort_slider", "Comfort: ", min = 0.08, max = 0.95,
+                                value = c(0.08, 0.95), step = 0.01),
+                    sliderInput("cost_slider", "Cost: ", min = 770, max = 3100,
+                                value = c(770, 3100), step = 10),
+                    selectInput("sites_excl", "Exclude sites manually by site ID:", multiple = TRUE,
+                                choices = sort(unique(as.character(final_table$site_id)))),
+                    ),
+            
+                # tab two containing the weight selections
+                tabPanel("Weights",
+                         br(),
+                         sliderInput("slider1", "Unemployment: ", min = 0, max = 100, value = 50, step = 1),
+                         sliderInput("slider2", "High school graduation rate: ", min = 0, max = 100, value = 50, step = 1),
+                         sliderInput("slider3", "Income: ", min = 0, max = 100, value = 50, step = 1),
+                         sliderInput("slider4", "Comfort: ", min = 0, max = 100, value = 50, step = 1),
+                         sliderInput("slider4", "Site cost: ", min = 0, max = 100, value = 50, step = 1)
 
             ))),
         
@@ -195,50 +167,61 @@ ui <- fluidPage(
             
         # Output: Tabset w/ plot, summary, and table ----
         tabsetPanel(type = "tabs",
-                    tabPanel("Plots", plotOutput("scatter")), # height = "500px"),
+                    tabPanel("Plots", plotOutput("densities")), # height = "500px"),
                     tabPanel("Table of selected sites", DT::dataTableOutput('table')), # height = "500px"),
                     tabPanel("Summary plots", plotOutput("boxplots")))
-                    # tabPanel("Table", tableOutput("table"))
 
-        # Show a plot of the generated distribution
-        # mainPanel(DT::dataTableOutput('table'), # height = "500px")
-        #           plotOutput("boxplots"))
     )
-
-    # text for bottom of the page
-    # HTML(
-    #     '<div class="belowplot" >
-    #      <p>Probability estimates based on 5,000 simulations @ alpha = 0.05</p>
-    #      <p>See
-    #      <a href="https://www.marlo.works/posts/a-b-testing/" target="_blank">marlo.works/A-B-Testing</a>
-    #       for more info</p>
-    #      </div>'
-    # )
 )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-
-
-    # the table
-    output$table <- DT::renderDataTable(DT::datatable({
+    
+    # number of current sites selected
+    output$n_sites_selected <- renderText({
+        
         data <- final_table
-
+        
         # filters
-        data <- data[data$region == input$region_slider,]
-        data <- data[data$urban == input$urban_slider,]
-        data <- data[data$other_prog == input$other_program_slider,]
+        data <- data[data$region %in% input$region_slider,]
+        data <- data[data$urban %in% input$urban_slider,]
+        data <- data[data$other_prog %in% input$other_program_slider,]
         data <- data[data$unemp >= input$unemp_slider[1] & data$unemp <= input$unemp_slider[2],]
         data <- data[data$pct_hs >= input$hs_slider[1] & data$pct_hs <= input$hs_slider[2],]
         data <- data[data$income >= input$income_slider[1] & data$income <= input$income_slider[2],]
         data <- data[data$comfort >= input$comfort_slider[1] & data$comfort <= input$comfort_slider[2],]
         data <- data[data$cost >= input$cost_slider[1] & data$cost <= input$cost_slider[2],]
+        data <- data[!data$site_id %in% input$sites_excl,]
+        
+        paste0(
+            '<h4>',
+            nrow(data),
+            ' sites are currently selected to be approached</h4>'
+        )
+        
+    })
+    
+    
+    # the table
+    output$table <- DT::renderDataTable(DT::datatable({
+        data <- final_table
+
+        # filters
+        data <- data[data$region %in% input$region_slider,]
+        data <- data[data$urban %in% input$urban_slider,]
+        data <- data[data$other_prog %in% input$other_program_slider,]
+        data <- data[data$unemp >= input$unemp_slider[1] & data$unemp <= input$unemp_slider[2],]
+        data <- data[data$pct_hs >= input$hs_slider[1] & data$pct_hs <= input$hs_slider[2],]
+        data <- data[data$income >= input$income_slider[1] & data$income <= input$income_slider[2],]
+        data <- data[data$comfort >= input$comfort_slider[1] & data$comfort <= input$comfort_slider[2],]
+        data <- data[data$cost >= input$cost_slider[1] & data$cost <= input$cost_slider[2],]
+        data <- data[!data$site_id %in% input$sites_excl,]
 
         data
     }, options = list(
         # sets n observations shown
-        pageLength = 10,
+        pageLength = 20,
         # removes option to change n observations shown
         lengthChange = FALSE,
         # removes the search bar
@@ -247,7 +230,7 @@ server <- function(input, output, session) {
         formatRound(9, 0))
 
     # the plots
-    output$scatter <- renderPlot({
+    output$densities <- renderPlot({
         
         # filters
         data <- final_table
@@ -259,14 +242,32 @@ server <- function(input, output, session) {
         data <- data[data$income >= input$income_slider[1] & data$income <= input$income_slider[2],]
         data <- data[data$comfort >= input$comfort_slider[1] & data$comfort <= input$comfort_slider[2],]
         data <- data[data$cost >= input$cost_slider[1] & data$cost <= input$cost_slider[2],]
+        data <- data[!data$site_id %in% input$sites_excl,]
         
-        data %>% 
+        # density plot of numeric variables
+        p1 <- data %>% 
             select(unemp, pct_hs, income, comfort, cost) %>% 
             pivot_longer(cols = everything()) %>% 
             ggplot(aes(x = value)) +
             geom_density() +
-            facet_wrap(~name, scales = 'free')
-
+            facet_wrap(~name, scales = 'free') +
+            labs(x = NULL,
+                 y = NULL)
+        
+        # bar plot of categorical variables
+        p2 <- data %>% 
+            select(region, urban, other_prog) %>%
+            mutate(urban = as.character(urban),
+                   other_prog = as.character(other_prog)) %>% 
+            pivot_longer(cols = everything()) %>% 
+            ggplot(aes(x = value)) +
+            geom_bar() +
+            facet_wrap(~name, scales = 'free_x') +
+            theme(axis.text.x = element_text(angle = 40, hjust = 1))
+        
+        # render both plots vertically
+        grid.arrange(p1, p2, ncol = 1)
+        
     })
     
     # the summary plots
